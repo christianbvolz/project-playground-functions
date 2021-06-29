f7ee34ea12ea22b7927b2340d1dbd0f449d270dd
Christianbv <christianbvolz@gmail.com>
2021-06-29 19:03:56 -0300

    Requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -55,8 +55,16 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanceCat1 = Math.abs(mouse - cat1);
+  let distanceCat2 = Math.abs(mouse - cat2);
+  if (distanceCat1 > distanceCat2) {
+    return 'cat2';
+  }
+  if (distanceCat2 > distanceCat1) {
+    return 'cat1';
+  }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
