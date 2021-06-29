2ece02067b1d7da3521306deac3cf21e0c680752
Christianbv <christianbvolz@gmail.com>
2021-06-29 17:21:50 -0300

    Requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -35,8 +35,8 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3 + ties);
 }
 
 // Desafio 6
