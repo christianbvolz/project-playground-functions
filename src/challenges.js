f9af55c7e07a820d23a82ce686372e623640923d
Christianbv <christianbvolz@gmail.com>
2021-06-29 17:15:12 -0300

    Requisito 4

--- src/challenges.js
+++ src/challenges.js
@@ -30,8 +30,8 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return (`${array[array.length - 1]}, ${array[0]}`);
 }
 
 // Desafio 5
