932b55b385907bf08442df50343010107529ddc3
Christianbv <christianbvolz@gmail.com>
2021-06-29 21:06:36 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,10 +1,13 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable complexity */
+/* eslint-disable max-lines-per-function */
+/* eslint-disable sonarjs/cognitive-complexity */
 // Desafio 10
 function techList(array, nome) {
   if (array.length === 0) {
     return 'Vazio!';
   }
   let result = [];
-  // eslint-disable-next-line guard-for-in
   for (let index in array.sort()) {
     result[index] = {
       tech: array[index],
@@ -15,8 +18,26 @@ function techList(array, nome) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let count = 0;
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index in array) {
+    if (array[index] > 9 || array[index] < 0) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    for (let number of array) {
+      if (array[index] === number) {
+        count += 1;
+        if (count >= 3) {
+          return 'não é possível gerar um número de telefone com esses valores';
+        }
+      }
+    }
+    count = 0;
+  }
+  return (`(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`);
 }
 
 // Desafio 12
